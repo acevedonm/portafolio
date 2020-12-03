34e7621a62fa94b573524e28a90dd63493bf1fea
Nicolas Acevedo <acevedonm@gmail.com>
2020-12-03 15:28:23 -0300

    newFeatures

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,15 @@ class App extends Component {
   return (
 <div style={{height: '300px', position: 'relative'}}>
     <Layout style={{background: 'url(https://justresults.co.nz/wp-content/uploads/2015/10/web-developer-banner.png) center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
+        <Header transparent title="hola mundoo" style={{color: 'white'}}>
             <Navigation>
-                <a href="#">Link</a>
+                <a href="#">link 1</a>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title="Este es el titulo drawer">
             <Navigation>
                 <a href="#">Link</a>
                 <a href="#">Link</a>
