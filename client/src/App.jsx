5794e9966a0b3827d59dd1a9ccb5fe92d277c3d7
Nicolas Acevedo <acevedonm@gmail.com>
2021-02-02 17:33:42 -0300

    refactoring navbar y cambio de color burgerbutton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
 import { Link } from "react-router-dom";
 import style from "./app.module.css";
-import "./app.module.css"
+import "./style/mdl.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHandshake,
@@ -14,89 +14,114 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
+  
+  const itemsNavBar= [
+    {href: "",
+  icon: null,
+sizeIcon: "",
+label: "label"},
+{href: "",
+  icon: null,
+sizeIcon: "",
+label: "label"},
+{href: "",
+  icon: null,
+sizeIcon: "",
+label: "label"},
+{href: "",
+  icon: null,
+sizeIcon: "",
+label: "label"},
+  ]
+
+  const  navBar = ()=>{
+    return (
+      <Layout>
+      <Header
+        className={style.navBar}
+        title={
+          <Link to="/" className={style.link}>
+            Acevedo Nicolás Portfolio
+          </Link>
+        }
+        scroll
+      >
+        <Navigation style={{display: "flex", justifyContent: "center"}}>
+          <Link
+            to="/certifications"
+            className={style.link}
+          >
+            <div style={{ background: "red",display: "flex" , justifyContent: "center", alignItems: "center"}}>
+            <FontAwesomeIcon
+              icon={faHome}
+              size="1x"
+              style={{ marginRight: "1px", paddingRight: "1px" }}
+            />  
+            Certifications
+            </div>
+          </Link>
+          <Link
+            to="/aboutme"
+            className={style.link}
+          >
+            <div style={{ background: "red",display: "flex" , justifyContent: "center", alignItems: "center"}}>
+            <FontAwesomeIcon
+              icon={faUser}
+              size="1x"
+              style={{ marginRight: "1px", paddingRight: "1px" }}
+            />
+            About Me
+            </div>
+          </Link>
+          <Link
+            to="/projects"
+            className={style.link}
+          >
+             <div style={{ background: "red",display: "flex" , justifyContent: "center", alignItems: "center"}}>
+            <FontAwesomeIcon
+              icon={faLaptopCode}
+              size="1x"
+              style={{ margin: "1px", padding: "1px" }}
+            />
+            Projects
+            </div>
+          </Link>
+          <Link
+            to="/contact"
+            className={style.link}
+          >
+            <div style={{display: "flex" , justifyContent: "center", alignItems: "center"}}>
+            <FontAwesomeIcon
+              icon={faHandshake}
+              size="1x"
+              style={{ margin: "1px", padding: "1px" }}
+            />
+            Contact
+            </div>
+          </Link>
+        </Navigation>
+      </Header>
+      <Drawer className={style.drawer}>
+        <Navigation>
+          <Link to="/" className={style.link}>Home</Link>  
+          <Link to="/certifications" className={style.link}>Certifications</Link>
+          <Link to="/aboutme" className={style.link}>About Me</Link>
+          <Link to="/projects" className={style.link}>Projects</Link>
+          <Link to="/contact" className={style.link}>Contact</Link>
+        </Navigation>
+      </Drawer>
+      <Content>
+        {/* INSERTO EL ROUTER EN EL CONTENIDO DE LA WEB */}
+        <div className="page-content" />
+        <Main />
+      </Content>
+    </Layout>
+    )
+  }
 
   return (
     <div>
-      <Layout>
-        <Header
-          className={style.navBar}
-          title={
-            <Link to="/" className={style.link}>
-              Acevedo Nicolás Portfolio
-            </Link>
-          }
-          scroll
-        >
-          <Navigation style={{display: "flex", justifyContent: "center"}}>
-            <Link
-              to="/certifications"
-              className={style.link}
-            >
-              <div style={{ background: "red",display: "flex" , justifyContent: "center", alignItems: "center"}}>
-              <FontAwesomeIcon
-                icon={faHome}
-                size="1x"
-                style={{ marginRight: "1px", paddingRight: "1px" }}
-              />  
-              Certifications
-              </div>
-            </Link>
-            <Link
-              to="/aboutme"
-              className={style.link}
-            >
-              <div style={{ background: "red",display: "flex" , justifyContent: "center", alignItems: "center"}}>
-              <FontAwesomeIcon
-                icon={faUser}
-                size="1x"
-                style={{ marginRight: "1px", paddingRight: "1px" }}
-              />
-              About Me
-              </div>
-            </Link>
-            <Link
-              to="/projects"
-              className={style.link}
-            >
-               <div style={{ background: "red",display: "flex" , justifyContent: "center", alignItems: "center"}}>
-              <FontAwesomeIcon
-                icon={faLaptopCode}
-                size="1x"
-                style={{ margin: "1px", padding: "1px" }}
-              />
-              Projects
-              </div>
-            </Link>
-            <Link
-              to="/contact"
-              className={style.link}
-            >
-              <div style={{display: "flex" , justifyContent: "center", alignItems: "center"}}>
-              <FontAwesomeIcon
-                icon={faHandshake}
-                size="1x"
-                style={{ margin: "1px", padding: "1px" }}
-              />
-              Contact
-              </div>
-            </Link>
-          </Navigation>
-        </Header>
-        <Drawer className={style.drawer}>
-          <Navigation>
-            <Link to="/" className={style.link}>Home</Link>  
-            <Link to="/certifications" className={style.link}>Certifications</Link>
-            <Link to="/aboutme" className={style.link}>About Me</Link>
-            <Link to="/projects" className={style.link}>Projects</Link>
-            <Link to="/contact" className={style.link}>Contact</Link>
-          </Navigation>
-        </Drawer>
-        <Content>
-          {/* INSERTO EL ROUTER EN EL CONTENIDO DE LA WEB */}
-          <div className="page-content" />
-          <Main />
-        </Content>
-      </Layout>
+      {navBar()}
     </div>
   );
 };
