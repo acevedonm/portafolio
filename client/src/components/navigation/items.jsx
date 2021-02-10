c19b984e82039a07dbdba2dbcfc249eacf38e686
Nicolas Acevedo <acevedonm@gmail.com>
2021-02-10 15:42:20 -0300

    elimine algunos items del navbar

--- client/src/components/navigation/items.jsx
+++ client/src/components/navigation/items.jsx
@@ -13,15 +13,15 @@ import {
     sizeIcon: "1x",
     label: "Home",
   },
-  {
+/*   {
     href: "/portafolio/education",
     icon: faUserGraduate,
     sizeIcon: "1x",
     label: "Education",
-  },
+  }, */
   { href: "/portafolio/aboutme", icon: faUser, sizeIcon: "1x", label: "About Me" },
   { href: "/portafolio/projects", icon: faLaptopCode, sizeIcon: "1x", label: "Projects" },
-  { href: "/portafolio/contact", icon: faHandshake, sizeIcon: "1x", label: "Contact" },
+  /* { href: "/portafolio/contact", icon: faHandshake, sizeIcon: "1x", label: "Contact" }, */
 ];
 
 export default items;
