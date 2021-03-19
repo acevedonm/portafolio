1728087bb8a0a4d77596c8e647cb2ae352307371
nico acevedo <acevedonm@gmail.com>
2021-03-18 22:49:31 -0300

    new tech

--- client/src/components/projects/data/techs.jsx
+++ client/src/components/projects/data/techs.jsx
@@ -10,9 +10,13 @@ import boostrapSVG from "../../../images/techs/boostrap.svg";
 import CSSSVG from "../../../images/techs/css.svg";
 import materialUISVG from "../../../images/techs/mdl.png";
 import firebaseSVG from "../../../images/techs/firebase.svg"
+import JavaSVG from "../../../images/techs/java.svg"
+import JavascriptSVG from "../../../images/techs/javascript.svg"
 
 const techs = {
   scrum: { name: "SCRUM", ico: scrumSVG },
+  java: {name: "Java", ico: JavaSVG},
+  javacript: {name: "Javascript", ico: JavascriptSVG},
   react: { name: "React", ico: reactSVG },
   redux: { name: "Redux", ico: reduxSVG },
   nodeJS: { name: "NodeJS", ico: nodeJSSVG },
@@ -24,6 +28,7 @@ const techs = {
   css: { name: "CSS", ico: CSSSVG },
   mdl: { name: "MaterialUI", ico: materialUISVG },
   firebase: { name: "Firebase", ico: firebaseSVG },
+  
 };
 
 export default techs;
