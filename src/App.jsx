e954b29d6c893bc316caad18a7f154a5753d65b7
nico acevedo <acevedonm@gmail.com>
2021-04-16 15:50:13 -0300

    restructuracion de front v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { Layout, Content } from "react-mdl";
+import { Layout, Content,Footer,FooterSection,FooterLinkList } from "react-mdl";
 import Main from "./components/main.jsx";
 import "./style/mdl.css";
 import NavBar from "./components/navigation/navBar/navBar";
 import DrawerNavigation from "./components/navigation/drawerNavigation/drawerNavigation";
+
+
+
 const App = () => {
   return (
     <div>
       <Layout>
         <NavBar />
-        <DrawerNavigation/>
+        <DrawerNavigation />
         <Content>
           {/* INSERTO EL ROUTER EN EL CONTENIDO DE LA WEB */}
-          
+
           <Main />
         </Content>
+        <Footer size="mini"  style={{backgroundColor:"white"}}>
+          <FooterSection type="left" logo="Titulo o logo">
+            <FooterLinkList>
+              <a href="#">Help</a>
+              <a href="#">Privacy & Terms</a>
+            </FooterLinkList>
+          </FooterSection>
+        </Footer>
       </Layout>
     </div>
   );
--- src/components/navigation/navBar/navBar.jsx
+++ src/components/navigation/navBar/navBar.jsx
@@ -1,48 +1,50 @@
 import React, { useState } from "react";
-import { Header, Navigation } from "react-mdl";
+import { Header, Navigation,HeaderRow } from "react-mdl";
 import { Link } from "react-router-dom";
 import style from "./navBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import items from "../items";
+import logo from "../../../images/logo.png";
 
 const NavBar = () => {
   const [activeSection, setActiveSection] = useState("Home");
 
   return (
-    <Header
-      className={style.navBar}
-      title={
-        <Link to="/" className={style.link}>
-          Acevedo Nicolás Portfolio
-        </Link>
-      }
-      transparent
-      scroll
-    >
-      <Navigation style={{ display: "flex", justifyContent: "center" }}>
-        {items.map((item, i) => {
-          return (
-            <Link
-              to={item.href}
-              className={
-                activeSection == item.label ? style.linkActive : style.link
-              }
-              key={i}
-              onClick={() => setActiveSection(item.label)}
-            >
-              <div className={style.navBarItem}>
-                <FontAwesomeIcon
-                  icon={item.icon}
-                  size={item.sizeIcon}
-                  style={{ marginRight: "1px", paddingRight: "1px" }}
-                />
-                {item.label}
-              </div>
-            </Link>
-          );
-        })}
-      </Navigation>
-    </Header>
+      <Header
+        className={style.navBar}
+        title={
+          <Link to="/" className={style.logo}>
+            ACEVEDO NICOLAS
+          </Link>
+        }
+        transparent
+        scroll
+        style={{paddingLeft: "15%"}}
+      >
+        <Navigation  style={{paddingRight: "15%"}}>
+          {items.map((item, i) => {
+            return (
+              <Link
+                to={item.href}
+                className={
+                  activeSection == item.label ? style.linkActive : style.link
+                }
+                key={i}
+                onClick={() => setActiveSection(item.label)}
+              >
+                <div className={style.navBarItem}>
+                  <FontAwesomeIcon
+                    icon={item.icon}
+                    size={item.sizeIcon}
+                    style={{ marginRight: "1px", paddingRight: "1px" }}
+                  />
+                  {item.label}
+                </div>
+              </Link>
+            );
+          })}
+        </Navigation>
+      </Header>
   );
 };
 
